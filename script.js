fbfae27c32900f5ac541a7a86535b13e307b3174
DiegoJfsr <diegojfsr@gmail.com>
2024-07-15 19:31:22 -0300

    Usando regex

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ function isValidDate(dateString){
     
 console.log(isValidDate("2020-01-02"));
 console.log(isValidDate("29328238"));
+
+
+
+// 2-usando regex
+
+function isValidDateRegex(dateString){
+    const pattern = /^\d{2}\/\d{2}\/\d{4}$/;
+    return pattern.test(dateString);
+}
+console.log(isValidDateRegex("2020-01-02"));
+console.log(isValidDateRegex("29328238"));
+console.log(isValidDateRegex("28/03/2023"));
+
